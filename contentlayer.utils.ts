d248594e961287ad6cf5f8a1d8b22b89d9e9cae3
laxko <ioelaxman@gmail.com>
2025-08-07 22:43:02 +0200

    utility functions more

--- contentlayer.utils.ts
+++ contentlayer.utils.ts
@@ -2,21 +2,30 @@ import { DocumentTypes } from '.contentlayer/generated'
 import { allBlogs, allAuthors } from '.contentlayer/generated'
 import type { Blog, Authors } from '.contentlayer/generated'
 import translationsText from '@/data/translations.json'
+import { headers } from 'next/headers'
 
 import siteMetadata from '@/data/siteMetadata'
 
-export const LOCALES = siteMetadata.locales
-export const LANGUAGE_COUNTRY_MATCH_REGEX = /^\/([a-z]{2})(?:\/([a-z]{2}))?(?=\/|$)/
-export const LOCALE_HEADER = 'x-locale'
+const LOCALES = siteMetadata.locales
+const LANGUAGE_COUNTRY_MATCH_REGEX = /^\/([a-z]{2})(?:\/([a-z]{2}))?(?=\/|$)/
+const LOCALE_HEADER = 'x-locale'
 
-export type Locale = keyof typeof LOCALES
-export type LocaleName = (typeof LOCALES)[Locale]
+type Locale = keyof typeof LOCALES
+type LocaleName = (typeof LOCALES)[Locale]
 
-export const isValidLocale = (locale: string): boolean => {
+type LocalizedSiteMetadata = typeof siteMetadata
+
+const getCurrentLocale = async (): Promise<Locale> => {
+  const locale = (await headers()).get(LOCALE_HEADER)
+  if (!locale || !isValidLocale(locale)) throw new Error('Invalid locale page request:' + locale)
+  return locale
+}
+
+const isValidLocale = (locale: string): boolean => {
   return Object.keys(LOCALES).includes(locale)
 }
 
-export const getLocaleByPathname = (pathname: string): Locale | 400 | null => {
+const getLocaleByPathname = (pathname: string): Locale | 400 | null => {
   // match /us/en/ (country/language) or /en/ (language)
   const [_, countryOrLanguageCode, languageCode] =
     pathname.match(LANGUAGE_COUNTRY_MATCH_REGEX) ?? []
@@ -32,32 +41,23 @@ export const getLocaleByPathname = (pathname: string): Locale | 400 | null => {
   return locale
 }
 
-export type NextPageProps = {
-  params?: Promise<{ slug?: string[]; page?: string; tag?: string }>
-  searchParams: Promise<{ locale: Locale }>
-}
-
-const isNextPageProps = (propsOrLocale: NextPageProps | Locale): propsOrLocale is NextPageProps => {
-  return 'searchParams' in (propsOrLocale as NextPageProps)
-}
-
-export const getAllContentByLocale = (content: DocumentTypes[], locale: Locale) => {
+const getAllContentByLocale = (content: DocumentTypes[], locale: Locale) => {
   return content.filter((item) => item.locale === locale) as DocumentTypes[]
 }
 
-export const getAllBlogsByLocale = async (props: NextPageProps): Promise<Blog[]> => {
-  const locale = (await props.searchParams).locale
+const getAllBlogs = async (): Promise<Blog[]> => {
+  const locale = await getCurrentLocale()
   return getAllContentByLocale(allBlogs as Blog[], locale) as Blog[]
 }
 
-export const getAllAuthorsByLocale = async (props: NextPageProps): Promise<Authors[]> => {
-  const locale = (await props.searchParams).locale
+const getAllAuthors = async (): Promise<Authors[]> => {
+  const locale = await getCurrentLocale()
   return getAllContentByLocale(allAuthors as Authors[], locale) as Authors[]
 }
 
 type TranslateFn = (text: keyof typeof translationsText, ...args: (string | number)[]) => string
 
-export const translate = (locale: Locale) => {
+const getTranslation = (locale: Locale) => {
   const translateFn: TranslateFn = (text, ...args) => {
     const template = translationsText[text][locale] || text
 
@@ -70,11 +70,26 @@ export const translate = (locale: Locale) => {
   return translateFn
 }
 
-export const translatePage = async (props: NextPageProps): Promise<TranslateFn> =>
-  props.searchParams.then((searchParams) => {
-    const locale = searchParams.locale
-    return translate(locale)
-  })
+const getTranslationPage = async (): Promise<TranslateFn> => {
+  const locale = await getCurrentLocale()
+  return getTranslation(locale)
+}
+
+const getSiteMetadata = async (): Promise<LocalizedSiteMetadata> => {
+  const locale = await getCurrentLocale()
+  return siteMetadata[locale]
+}
 
 export { allBlogs, allAuthors } from '.contentlayer/generated'
 export type { Blog, Authors } from '.contentlayer/generated'
+
+export {
+  getLocaleByPathname,
+  getAllBlogs,
+  getAllAuthors,
+  getTranslation,
+  getTranslationPage,
+  getSiteMetadata,
+}
+
+export type { Locale, LocaleName, TranslateFn }
