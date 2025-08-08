134a7964577112458710c20b6968941313b3e651
laxko <ioelaxman@gmail.com>
2025-08-09 00:08:18 +0200

    maybe final x

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -21,7 +21,9 @@ const generateRssItem = (config, post) => `
   </item>
 `
 
-const generateRss = (config, posts, page = 'feed.xml') => `
+const generateRss = (config, posts, page = 'feed.xml') => {
+  if (!posts.length) return ''
+  return `
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
       <title>${escape(config.title)}</title>
@@ -36,6 +38,7 @@ const generateRss = (config, posts, page = 'feed.xml') => `
     </channel>
   </rss>
 `
+}
 
 async function generateRSS(config, allBlogs, page = 'feed.xml') {
   const publishPosts = allBlogs.filter((post) => post.draft !== true)
@@ -57,7 +60,7 @@ async function generateRSS(config, allBlogs, page = 'feed.xml') {
 }
 
 const rss = () => {
-  generateRSS(siteMetadata, allBlogs)
+  generateRSS(siteMetadata.en, allBlogs)
   console.log('RSS feed generated...')
 }
 export default rss
