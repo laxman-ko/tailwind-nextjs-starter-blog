e0410c6ddea182ddaf8622d522f1d7b0a13cc82f
Laxman Siwakoti <laxmansiwakoti@Laxmans-MBP.fritz.box>
2025-08-13 13:12:28 +0200

    page runtime

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import Main from './Main'
 import { getAllBlogs } from 'app/contentlayer.utils.server'
 
+export const runtime = 'edge'
+
 export default async function Page() {
   const allBlogs = await getAllBlogs()
   const sortedPosts = sortPosts(allBlogs)
