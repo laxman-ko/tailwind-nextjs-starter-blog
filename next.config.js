a09c0fbada9149ea393045043b61569090bb3370
Laxman Siwakoti <laxmansiwakoti@Laxmans-MBP.fritz.box>
2025-08-17 23:13:54 +0200

    git hook

--- next.config.js
+++ next.config.js
@@ -95,6 +95,8 @@ module.exports = () => {
         use: ['@svgr/webpack'],
       })
 
+      config.resolve.alias.querystring = require.resolve('querystring-es3')
+
       return config
     },
   })
