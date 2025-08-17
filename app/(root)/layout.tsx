a366d498dfe6729c137eb33b618e924ee4052857
Laxman Siwakoti <laxmansiwakoti@Laxmans-MBP.fritz.box>
2025-08-17 13:15:26 +0200

    under construction

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -63,14 +63,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const basePath = process.env.BASE_PATH || ''
 
-  if (siteMetadata.isUnderConstruction) {
-    // return (
-    //   <html lang={siteMetadata.language}>
-    //     <body>
-    //       <h1>{_t('Under Construction')}</h1>
-    //     </body>
-    //   </html>
-    // )
+  if (siteMetadata.isUnderConstruction && process.env.CF_PAGES_BRANCH === 'main') {
+    return (
+      <html lang={siteMetadata.language}>
+        <body>
+          <h1>{_t('Under Construction')}</h1>
+        </body>
+      </html>
+    )
   }
 
   return (
